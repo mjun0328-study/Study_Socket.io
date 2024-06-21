97ad32ee1b2d5641813be653a972991df81c4c58
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2024-06-21 19:28:35 +0900

    socket.io의 이해와 양방향 통신의 구축

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,32 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
 const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    mathods: ["GET", "POST"],
+  },
+});
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("upload_msg", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+app.use(function (req, res, next) {
+  res.io = io;
+  next();
+});
 
 app.use("/", require("./routes/index"));
 
@@ -21,6 +39,8 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  console.log(err);
+
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
@@ -30,4 +50,4 @@ app.use(function (err, req, res, next) {
   res.send("Boom!");
 });
 
-module.exports = app;
+module.exports = { app, server };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,50 @@
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  const socket = io.connect("ws://localhost:8080");
+
+  const [receive, setReceive] = useState("");
+  const [send, setSend] = useState("");
+
+  useEffect(() => {
+    const connect = () => {
+      console.log("Connected!");
+    };
+
+    const disconnect = () => {
+      console.log("Disconnected!");
+    };
+
+    const download = (data) => {
+      console.log("Downloaded:", data);
+      setReceive(data);
+    };
+
+    socket.on("connect", connect);
+    socket.on("disconnect", disconnect);
+    socket.on("download_msg", download);
+
+    return () => {
+      socket.off("connect", connect);
+      socket.off("disconnect", disconnect);
+      socket.off("download_msg", download);
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  const onSend = () => {
+    socket.emit("upload_msg", send);
+  };
+
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <p>{receive}</p>
+      <input value={send} onChange={(e) => setSend(e.target.value)} />
+      <button onClick={onSend}>Send</button>
+    </>
+  );
 }
 
 export default App;
